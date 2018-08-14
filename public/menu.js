14dacb8c549649e225f6b4fd31cf861cc370fe2f
Ruel Misa <ruel.g.misa@gmail.com>
2018-08-14 13:54:27 -0400

    fixed the nav menu. somehow, I had to remake my js file to get it to work AGAIN

--- public/menu.js
+++ public/menu.js
@@ -1,9 +1,11 @@
+console.log('Hello darkness');
+
 let openMenu = () => {
     // alert('working');
     document.getElementById('popMenu').style.width = "100%";
 }
 
 let closeMenu = () => {
-    alert('are you working?');
+    // alert('are you working?');
     document.getElementById('popMenu').style.width = "0";
 }
